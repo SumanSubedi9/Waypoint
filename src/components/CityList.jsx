6358658329346dcd5cac1ec0a00b05e6644b74d9
Suman  Subedi <ssubedi60@gmail.com>
2024-10-24 19:54:49 -0500

    Implemented URL state and useNavigate on the application

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -2,9 +2,13 @@
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 import CityItem from "./CityItem";
+import Message from "./Message";
 
 function CityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return <Message message="Get Started by clicking on a city on the map" />;
   return (
     <ul className={styles.cityList}>
       {cities.map((city) => (
