31cb34ca967bd451a1849a219b4685932dfe9e4a
Suman  Subedi <ssubedi60@gmail.com>
2024-11-06 18:49:20 -0600

    Implemented useCallback hook to cache getCity function between re-renders, with useCallback the function is fetching infinite requests

--- src/context/CitiesContext.jsx
+++ src/context/CitiesContext.jsx
@@ -1,5 +1,11 @@
 /* eslint-disable react-refresh/only-export-components */
-import { createContext, useContext, useEffect, useState } from "react";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
 const CitiesContext = createContext();
 
@@ -26,7 +32,7 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
-  async function getCity(id) {
+  const getCity = useCallback(async function getCity(id) {
     try {
       setIsLoading(true);
       const res = await fetch(`${BASE_URL}/cities/${id}`);
@@ -37,7 +43,7 @@ function CitiesProvider({ children }) {
     } finally {
       setIsLoading(false);
     }
-  }
+  }, []);
 
   async function createCity(newCity) {
     try {
