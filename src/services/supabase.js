272edae09488a7c3fffbe067ef9b12e9de7cd5f5
Suman  Subedi <ssubedi60@gmail.com>
2024-12-03 19:30:00 -0600

    Secured the supabase url and key

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://cvkjpwghtllilrrjbpyr.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2a2pwd2dodGxsaWxycmpicHlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIyMjY4NjIsImV4cCI6MjA0NzgwMjg2Mn0.eBLdWI28agLvO9WazwkD0SgDdWnJKL5ZWrDspK4x5zQ";
+
+require("dotenv").config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
