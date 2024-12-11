5a648a3c9693aeea1b1f4327a1b01a0e885f3930
Suman  Subedi <ssubedi60@gmail.com>
2024-12-11 16:17:17 -0600

    successfully moved the database to supabase

--- src/context/CitiesContext.jsx
+++ src/context/CitiesContext.jsx
@@ -7,9 +7,11 @@ import {
   useState,
 } from "react";
 
+import supabase from "../services/supabase.js";
+
 const CitiesContext = createContext();
 
-const BASE_URL = "http://localhost:9000";
+// const BASE_URL = "http://localhost:9000";
 
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
@@ -20,8 +22,13 @@ function CitiesProvider({ children }) {
     async function fetchCities() {
       try {
         setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
+        const { data, error } = await supabase.from("cities").select("*");
+        if (error) throw new Error("Cities could not be retrieved");
+        // const res = await fetch(`${BASE_URL}/cities`);
+        // const data = await res.json();
+
+        console.log(data);
+
         setCities(data);
       } catch {
         alert("There was an error loading data...");
@@ -35,8 +42,11 @@ function CitiesProvider({ children }) {
   const getCity = useCallback(async function getCity(id) {
     try {
       setIsLoading(true);
-      const res = await fetch(`${BASE_URL}/cities/${id}`);
-      const data = await res.json();
+      const { data, error } = await supabase.from("cities").select("id", id);
+
+      if (error) throw new Error("City could not be retrieved");
+      // const res = await fetch(`${BASE_URL}/cities/${id}`);
+      // const data = await res.json();
       setCurrentCity(data);
     } catch {
       alert("There was an error loading data...");
@@ -48,14 +58,28 @@ function CitiesProvider({ children }) {
   async function createCity(newCity) {
     try {
       setIsLoading(true);
-      const res = await fetch(`${BASE_URL}/cities`, {
-        method: "POST",
-        body: JSON.stringify(newCity),
-        headers: {
-          "Content-Type": "application/json",
+      const { data, error } = await supabase.from("cities").insert({
+        cityName: newCity.cityName,
+        country: newCity.country,
+        emoji: newCity.emoji,
+        date: newCity.date,
+        notes: newCity.notes,
+        position: {
+          lat: newCity.position.lat,
+          lng: newCity.position.lng,
         },
       });
-      const data = await res.json();
+
+      if (error) console.error("Error inserting data:", error);
+      if (data) console.log("Successfully inserted:", data);
+      // const res = await fetch(`${BASE_URL}/cities`, {
+      //   method: "POST",
+      //   body: JSON.stringify(newCity),
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      // });
+      // const data = await res.json();
       setCities((cities) => [...cities, data]);
     } catch {
       alert("There was an error while creating the city.");
@@ -67,11 +91,16 @@ function CitiesProvider({ children }) {
   async function deleteCity(id) {
     try {
       setIsLoading(true);
-      await fetch(`${BASE_URL}/cities/${id}`, {
-        method: "DELETE",
-      });
+      const { error } = await supabase.from("cities").delete().eq("id", id);
 
-      setCities((cities) => cities.filter((city) => city.id !== id));
+      if (error) console.error("Error deleting data:", error);
+      // await fetch(`${BASE_URL}/cities/${id}`, {
+      //   method: "DELETE",
+      // });
+
+      setCities((cities) =>
+        cities.filter((city) => city !== null && city.id !== id)
+      );
     } catch {
       alert("There was an error while deleting the city.");
     } finally {
