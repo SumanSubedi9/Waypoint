5a1c0cdc248ccc981c3f50b65e0229e9a7df738e
Suman  Subedi <ssubedi60@gmail.com>
2024-12-12 21:31:37 -0600

    after successfull signup, the application will navigate to the login page

--- src/authentication/useSignup.js
+++ src/authentication/useSignup.js
@@ -1,12 +1,15 @@
 import { useMutation } from "@tanstack/react-query";
 import { signup as signupApi } from "../services/apiAuth.js";
 import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 export function useSignup() {
+  const navigate = useNavigate();
   const { mutate: signup, isLoading } = useMutation({
     mutationFn: signupApi,
     onSuccess: (user) => {
       toast.success("Account successfully created!");
+      navigate("/login");
     },
   });
 
