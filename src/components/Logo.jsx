a20344993c5eedd2f3241874c80bfb1d2e79f7cd
Suman  Subedi <ssubedi60@gmail.com>
2024-12-17 10:41:47 -0600

    destination change for logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 import styles from "./Logo.module.css";
+import logo from "/public/Logo.png";
 function Logo() {
   return (
     <Link to="/">
-      <img src="/public/Logo.png" alt="Waypoint logo" className={styles.logo} />
+      <img src={logo} alt="Waypoint logo" className={styles.logo} />
     </Link>
   );
 }
