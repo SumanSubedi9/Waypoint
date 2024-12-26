dfad544d49a76c74efe1d77130fd8c760aee31ee
Suman  Subedi <ssubedi60@gmail.com>
2024-12-25 21:52:20 -0600

    Implemented the mobile navigation menu with react-icons

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,20 +1,58 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
 import Logo from "./Logo";
+import { IoClose } from "react-icons/io5";
+import { IoMenu } from "react-icons/io5";
 
 function PageNav() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className={styles.nav}>
-      <Logo />
-      <ul>
+    <>
+      <nav className={styles.nav}>
+        <Logo />
+        {/* Desktop Nav*/}
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Explore">Explore</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Memories">Memories</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login" className={styles.ctaLink}>
+              Login
+            </NavLink>
+          </li>
+        </ul>
+        {isOpen ? (
+          <IoClose
+            className={styles.menuCloseBtn}
+            onClick={() => setIsOpen(false)}
+          />
+        ) : (
+          <IoMenu className={styles.menuBtn} onClick={() => setIsOpen(true)} />
+        )}
+      </nav>
+
+      {/*Mobile Navigation*/}
+      <ul
+        className={`${styles.mobileNav} ${
+          isOpen ? styles.menuOpen : styles.menuClosed
+        } `}
+        onClick={() => setIsOpen(false)}
+      >
         <li>
           <NavLink to="/">Home</NavLink>
         </li>
         <li>
-          <NavLink to="/product">Explore</NavLink>
+          <NavLink to="/Explore">Explore</NavLink>
         </li>
         <li>
-          <NavLink to="/pricing">Memories</NavLink>
+          <NavLink to="/Memories">Memories</NavLink>
         </li>
         <li>
           <NavLink to="/login" className={styles.ctaLink}>
@@ -22,7 +60,7 @@ function PageNav() {
           </NavLink>
         </li>
       </ul>
-    </nav>
+    </>
   );
 }
 
