68f19cabcaa5a353f10c1263c8d2d146e6815564
Suman  Subedi <ssubedi60@gmail.com>
2025-01-20 18:48:59 -0600

    finalized the explore page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,11 +1,19 @@
 import PageNav from "../components/PageNav.jsx";
 import styles from "./Explore.module.css";
+import Gallery from "../components/Gallery/Gallery.jsx";
+import Testimonial from "../components/Testimonials/Testimonial.jsx";
 
 function Explore() {
   return (
     <section className={styles.explore}>
       <PageNav />
-      <h1>Explore</h1>
+      <h1>
+        Your journey, our compass—wherever you travel, we&apos;ll track it.
+      </h1>
+      <div className={styles.exploreContainer}>
+        <Gallery />
+        <Testimonial />
+      </div>
     </section>
   );
 }
