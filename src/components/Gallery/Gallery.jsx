624c372be57f3e034ab9a95ce103b94270dfa170
Suman  Subedi <ssubedi60@gmail.com>
2025-01-26 15:46:32 -0600

    fixed some spacing issue on explore page, and added lazy loading on all the <img/>

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -7,54 +7,63 @@ function Gallery() {
         <img
           src="/gallery/gallery-1.jpg"
           alt="photo of arranged destinations"
+          loading="lazy"
         />
       </figure>
       <figure className={styles.galleryItem}>
         <img
           src="/gallery/gallery-2.jpg"
           alt="photo of arranged destinations"
+          loading="lazy"
         />
       </figure>
       <figure className={styles.galleryItem}>
         <img
           src="/gallery/gallery-3.jpg"
           alt="photo of arranged destinations"
+          loading="lazy"
         />
       </figure>
       <figure className={styles.galleryItem}>
         <img
           src="/gallery/gallery-4.jpg"
           alt="photo of arranged destinations"
+          loading="lazy"
         />
       </figure>
       <figure className={styles.galleryItem}>
         <img
           src="/gallery/gallery-5.jpg"
           alt="photo of arranged destinations"
+          loading="lazy"
         />
       </figure>
       <figure className={styles.galleryItem}>
         <img
           src="/gallery/gallery-6.jpg"
           alt="photo of arranged destinations"
+          loading="lazy"
         />
       </figure>
       <figure className={styles.galleryItem}>
         <img
           src="/gallery/gallery-7.jpg"
           alt="photo of arranged destinations"
+          loading="lazy"
         />
       </figure>
       <figure className={styles.galleryItem}>
         <img
           src="/gallery/gallery-8.jpg"
           alt="photo of arranged destinations"
+          loading="lazy"
         />
       </figure>
       <figure className={styles.galleryItem}>
         <img
           src="/gallery/gallery-9.jpg"
           alt="photo of arranged destinations"
+          loading="lazy"
         />
       </figure>
     </div>
