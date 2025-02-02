6f6d5be40725676c2f55b6bea22a988f3ad07a62
Suman  Subedi <ssubedi60@gmail.com>
2025-02-01 19:09:44 -0600

    added the button to the city component

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import { useCities } from "../context/CitiesContext";
 import Spinner from "./Spinner";
 import BackButton from "./BackButton";
+import ImageUpload from "../ui/UploadTravelPhotos/ImageUpload";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -25,41 +26,44 @@ function City() {
 
   if (isLoading) return <Spinner />;
   return (
-    <div className={styles.city}>
-      <div className={styles.row}>
-        <h6>City name</h6>
-        <h3>
-          <span>{emoji}</span> {cityName}
-        </h3>
-      </div>
-
-      <div className={styles.row}>
-        <h6>You went to {cityName} on</h6>
-        <p>{formatDate(date || null)}</p>
-      </div>
-
-      {notes && (
+    <>
+      <div className={styles.city}>
         <div className={styles.row}>
-          <h6>Your notes</h6>
-          <p>{notes}</p>
+          <h6>City name</h6>
+          <h3>
+            <span>{emoji}</span> {cityName}
+          </h3>
         </div>
-      )}
 
-      <div className={styles.row}>
-        <h6>Learn more</h6>
-        <a
-          href={`https://en.wikipedia.org/wiki/${cityName}`}
-          target="_blank"
-          rel="noreferrer"
-        >
-          Check out {cityName} on Wikipedia &rarr;
-        </a>
-      </div>
+        <div className={styles.row}>
+          <h6>You went to {cityName} on</h6>
+          <p>{formatDate(date || null)}</p>
+        </div>
 
-      <div>
-        <BackButton />
+        {notes && (
+          <div className={styles.row}>
+            <h6>Your notes</h6>
+            <p>{notes}</p>
+          </div>
+        )}
+
+        <div className={styles.row}>
+          <h6>Learn more</h6>
+          <a
+            href={`https://en.wikipedia.org/wiki/${cityName}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Check out {cityName} on Wikipedia &rarr;
+          </a>
+        </div>
+
+        <div>
+          <BackButton />
+        </div>
       </div>
-    </div>
+      <ImageUpload />
+    </>
   );
 }
 
