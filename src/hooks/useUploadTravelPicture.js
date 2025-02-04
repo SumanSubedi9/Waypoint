496e4fd2b4cb8058005b02413a724c944804b765
Suman  Subedi <ssubedi60@gmail.com>
2025-02-04 14:36:57 -0600

    commented cl

--- src/hooks/useUploadTravelPicture.js
+++ src/hooks/useUploadTravelPicture.js
@@ -7,9 +7,9 @@ export function useUploadTravelPicture() {
   return useMutation({
     mutationFn: ({ file, userId, id }) =>
       uploadUserTravelPicture(file, userId, id),
-    onSuccess: (data) => {
+    onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["cities"] });
-      console.log("Image successfully uploaded:", data);
+      // console.log("Image successfully uploaded:", data);
     },
     onError: (error) => {
       console.error("Image upload failed:", error);
