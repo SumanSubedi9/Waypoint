65f678a54cea2ee82c1f5196921419771a57e9c2
Suman  Subedi <ssubedi60@gmail.com>
2025-02-11 18:36:33 -0600

    temporary solution for stripe checkout in (test mode)

--- src/ui/ProButton/ProButton.jsx
+++ src/ui/ProButton/ProButton.jsx
@@ -2,9 +2,15 @@ import styles from "../ProButton/ProButton.module.css";
 import { PiLightningFill } from "react-icons/pi";
 
 function ProButton() {
+  const paymentLink = "https://buy.stripe.com/test_8wM2c50oTfv79EcbII";
+
+  function handleClick(e) {
+    e.preventDefault();
+    window.open(paymentLink);
+  }
   return (
     <div className={styles.container}>
-      <button className={styles.proBtn}>
+      <button onClick={handleClick} className={styles.proBtn}>
         Upgrade
         <PiLightningFill />
       </button>
