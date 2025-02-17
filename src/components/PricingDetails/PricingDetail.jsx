1f60ac213d1c2ba7953bd62d91ddbb17631d74c6
Suman  Subedi <ssubedi60@gmail.com>
2025-02-17 17:44:17 -0600

    removed coming soon tag

--- src/components/PricingDetails/PricingDetail.jsx
+++ src/components/PricingDetails/PricingDetail.jsx
@@ -80,7 +80,6 @@ function PricingDetail() {
           <li className={styles.listItem}>
             <IoCheckmark className={styles.checkMark} />
             <span>Personalized Trip Insights</span>
-            <div className={styles.comingSoon}>Coming Soon</div>
           </li>
         </ul>
         <Button onClick={() => handleClick("/login")} style="primary">
