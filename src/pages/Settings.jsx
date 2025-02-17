efd78fcb3933129b9448114a7a28c1d0e02ab15d
Suman  Subedi <ssubedi60@gmail.com>
2025-02-17 17:29:41 -0600

    added the waypoint logo that will take user back to /app changed the background color

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,5 +1,6 @@
 import styles from "./Settings.module.css";
 import User from "../components/User";
+import { Link } from "react-router-dom";
 import UpdateUserDataForm from "../authentication/UpdateUserInfo/UpdateUserDataForm";
 import UpdatePasswordForm from "../authentication/UpdateUserInfo/UpdatePasswordForm";
 import BackButton from "../components/BackButton";
@@ -7,6 +8,9 @@ import BackButton from "../components/BackButton";
 function Settings() {
   return (
     <div className={styles.page}>
+      <Link to="/app">
+        <img src="/Logo.png" alt="Waypoint logo" className={styles.logo} />
+      </Link>
       <h1>Update your account</h1>
       <div className={styles.setting}>
         <User />
