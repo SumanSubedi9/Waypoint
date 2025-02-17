81a11e2ea4fe1903a61b87ae0b6f2ea9573a70ad
Suman  Subedi <ssubedi60@gmail.com>
2025-02-17 17:06:18 -0600

    created statIcon

--- src/ui/StatIcon.jsx
+++ src/ui/StatIcon.jsx
@@ -1,9 +1,15 @@
 import styles from "./StatIcon.module.css";
+import { useNavigate } from "react-router-dom";
 import { BsClipboardData } from "react-icons/bs";
 
 function StatIcon() {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate("/user-stat");
+  }
   return (
-    <button className={styles.icon}>
+    <button className={styles.icon} onClick={handleClick}>
       <BsClipboardData />
     </button>
   );
